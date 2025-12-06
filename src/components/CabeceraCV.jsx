5884af782637bf7bbeccc1445617bc103ff74c2c
juancamilomontes <jm3876602@gmail.com>
2025-12-05 21:09:39 -0500

    feat:#2 CabeceraCV recibe props

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -1,20 +1,13 @@
-export default function CabeceraCV() {
+export default function CabeceraCV({nombre, cargo, ciudad, contacto}) {
   return (
     <header>
-      <h1>Juan Camilo Montes Velasquez</h1>
-      <h2>Estudiante tecnologo en analisis y desarrollo de sorftware</h2>
-      <h2>
-        <ul className="contact-list">
-          <li>
-            Teléfono: <a href="tel:+57 3247744901">+57 3247744901</a>
-          </li>
-          <li>
-            Correo:{" "}
-            <a href="coreo:jm3876602@gmail.com.com">jm3876602@gmail.com.com</a>
-          </li>
-          <li>Ciudad: Medellin</li>
-        </ul>
-      </h2>
+      <h1>{nombre}</h1>
+      <h2>{cargo}</h2>
+      <ul>
+        <li>Teléfono: {contacto.telefono}</li>
+        <li>Correo: {contacto.correo}</li>
+        <li>Ciudad: {ciudad}</li>
+      </ul>
     </header>
   );
 }
