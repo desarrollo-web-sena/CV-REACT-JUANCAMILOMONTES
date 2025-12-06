7d7bc23526392da4ada87cd0e238f873435aca56
juancamilomontes <jm3876602@gmail.com>
2025-12-05 21:15:33 -0500

    feat:#4 Perfil dinámico con props

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,18 +1,8 @@
-export default function Perfil() {
+export default function Perfil({resumen}) {
   return (
     <section>
       <h3>Perfil</h3>
-      <p>
-        Amante a la tecnología y el ámbito de la programación, con ganas de
-        aprender y crecer profesionalmente en el área del desarrollo de
-        software. Capaz de trabajar en equipo y bajo presión, con habilidades de
-        comunicación y adaptabilidad. Busco oportunidades para contribuir en
-        proyectos reales, aprender nuevas tecnologías como React, Node.js y
-        TypeScript, y aplicar buenas prácticas de desarrollo. Me interesa el
-        desarrollo front-end, crear interfaces accesibles y optimizadas, y
-        avanzar en mi formación mediante el trabajo colaborativo y la mejora
-        continua.
-      </p>
+      <p>{resumen}</p>
     </section>
   );
 }
